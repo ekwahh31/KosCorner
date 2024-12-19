e25cfa29928f6152ec020fe8fbbca7213e864c7c
ekwahh31 <ekabintangcoys@gmail.com>
2024-12-19 18:13:41 +0700

    tambah db buat hero

--- koscorner.sql
+++ koscorner.sql
@@ -10,3 +10,12 @@ INSERT INTO kos_recommendations (image_url, title, price, link) VALUES
 ('https://example.com/images/4.jpg', 'KOST GAVIN TIPE AC PURWOKERTO SELATAN', 'Rp1.000.000 / bulan', 'kos4.php'),
 ('https://example.com/images/5.jpg', 'Kost Pintu Biruku Purwokerto Timur Banyumas', 'Rp 1.400.000 / bulan', 'kos5.php'),
 ('https://example.com/images/6.jpg', 'Kost Griya Laras Ati Tipe Vip Purwokerto Timur', 'Rp 1.000.000 / bulan', 'kos6.php');
+
+CREATE TABLE hero_images (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    image_url VARCHAR(255) NOT NULL,
+    alt_text VARCHAR(255) NOT NULL
+);
+
+INSERT INTO hero_images (image_url, alt_text) VALUES
+('https://example.com/images/Kos-kosan.jpg', 'Hero Image');
