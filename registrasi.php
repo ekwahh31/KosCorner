7ccb2368fa7239cd76a8d1db215594a088c58a59
ekwahh31 <ekabintangcoys@gmail.com>
2024-12-20 10:22:24 +0700

    ubah navbar

--- registrasi.php
+++ registrasi.php
@@ -20,7 +20,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register Page</title>
-    <link rel="stylesheet" href="../css/registrasi.css">
+    <link rel="stylesheet" href="registrasi.css">
     <script defer src="register.js"></script>
 </head>
 <body>
