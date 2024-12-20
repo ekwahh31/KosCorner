faf5e5882b4b998a6915ee90d653cbdedad24e77
ekwahh31 <ekabintangcoys@gmail.com>
2024-12-20 10:35:28 +0700

    benerin href

--- login.php
+++ login.php
@@ -25,7 +25,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
-    <link rel="stylesheet" href="../style/login.css">
+    <link rel="stylesheet" href="style/login.css">
 </head>
 <body>
     <div class="login-container">
